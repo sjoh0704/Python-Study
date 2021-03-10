9355ac90d65c378c73cca8c1533da5c2215fb5ac
Seung <sjoh0704@daum.net>
2021-03-11 00:39:05 +0900

    add web write page

--- web_basic/django_basic/community/board/views.py
+++ web_basic/django_basic/community/board/views.py
@@ -1,14 +1,29 @@
-from web_basic.django_basic.community.board.form import BoardForm
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Board
 from .form import BoardForm
 # Create your views here.
 
 
 def board_write(request):
-    form = BoardForm()
-    return render(request, 'board_write.html', {"form": form})
+
+    if request.method == "POST":
+        form = BoardForm(request.POST)
+        if form.is_valid():
+
+            # request.session['user'] = form.user_id
+            title = request.POST.get("title", None)
+            contents = request.POST.get("contents", None)
+
+            board = Board(contents=contents, title=title,
+                          writer=None, registered_dttm=None)
+            board.save()
+
+            return redirect("/board/list")
+    else:
+
+        form = BoardForm()
+    return render(request, 'board_write.html', {'form': form})
 
 
 def board_list(request):
